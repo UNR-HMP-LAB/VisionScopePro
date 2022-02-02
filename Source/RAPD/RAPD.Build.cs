796290bbfa90cc48ad18e3c9113afac48cc72ca2
prithuls <prithul.pro@gmail.com>
2022-02-01 19:23:48 -0800

    fove eye tracking works now
    Signed-off-by: prithuls <prithul.pro@gmail.com>

--- Source/RAPD/RAPD.Build.cs
+++ Source/RAPD/RAPD.Build.cs
@@ -8,11 +8,11 @@ public class RAPD : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "SRanipalEye"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "SRanipalEye", "FoveHMD"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		PublicIncludePaths.AddRange(new string[] { "SRanipalEye/Public"});
+		PublicIncludePaths.AddRange(new string[] { }); //"SRanipalEye/Public"
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
